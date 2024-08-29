5b687e75760b5d9c8a8e39ee8ad554bc67bdd1a9
砍砍 <git@qaq.wiki>
2024-08-29 16:46:30 +0800

    Update Packaging

--- Resources/compile.release.rootless.sh
+++ Resources/compile.release.rootless.sh
@@ -101,7 +101,7 @@ sed -i '' "s/ENV_PREFIX=\"\"/ENV_PREFIX=\"\/var\/jb\/\"/g" ./DEBIAN/postinst
 sed -i '' "s/@@VERSION@@/2.1-REL-$TIMESTAMP/g" ./DEBIAN/control
 sed -i '' "s/iphoneos-arm/iphoneos-arm64/g" ./DEBIAN/control
 sed -i '' "s/Package: wiki.qaq.chromatic/Package: wiki.qaq.chromatic.rootless/g" ./DEBIAN/control
-sed -i '' "s/Name: Saily/Name: Saily - rootless (zp)/g" ./DEBIAN/control
+sed -i '' "s/Name: Saily/Name: Saily (Rootless)/g" ./DEBIAN/control
 mv ./DEBIAN/control ./DEBIAN/control_
 awk '{print} END{print "Conflicts: wiki.qaq.chromatic"}' ./DEBIAN/control_ > ./DEBIAN/control
 
